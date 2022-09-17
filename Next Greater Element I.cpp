a9ad6fb11631d6cd62cd5154b7192695fd040c7f
KALkidan-Tadu <93763525+KALkidan-Tadu@users.noreply.github.com>
2022-09-17 02:25:23 -0700

    Update Next Greater Element I.cpp

--- Next Greater Element I.cpp
+++ Next Greater Element I.cpp
@@ -1,6 +1,36 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> mymap;
+        stack<int> mystack;
+        for(int i=0; i<nums2.size(); i++){
+            if(mystack.empty() || mystack.top()>=nums2[i])
+                mystack.push(nums2[i]);
+            else{
+                while(!mystack.empty() && mystack.top()<nums2[i]){
+                    int n = mystack.top();
+                    mymap[n] = nums2[i];
+                    mystack.pop();
+                }
+                mystack.push(nums2[i]);
+            }
+            }
+        while(!mystack.empty()){
+                int n = mystack.top();
+                mymap[n] = -1;
+                mystack.pop();
+        }
+        for(int i=0; i<nums1.size(); i++){
+            nums1[i] = mymap[nums1[i]];
+        }
+        return nums1;
+    }
+};
+
+
+/*class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         int i=0;
         vector<int> answer(nums1.size());
         while(i<nums1.size()){
@@ -26,3 +56,4 @@ public:
         return answer;
     }
 };
+*/
