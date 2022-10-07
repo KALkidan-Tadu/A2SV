69bef17be954b84eea66bf0f04a6caf780ee5413
KALkidan-Tadu <93763525+KALkidan-Tadu@users.noreply.github.com>
2022-10-07 14:52:53 -0700

    Create Find the Student that Will Replace the Chalk.cpp

---
+++ Find the Student that Will Replace the Chalk.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int chalkReplacer(vector<int>& chalk, int k) {
+        long long sum = 0;
+        int index = 0;
+       for(int i=0; i<chalk.size(); i++)
+           sum += chalk[i];
+        k%=sum;
+        for(int i=0; i<chalk.size(); i++){
+            if(k<chalk[i]){
+                index = i;
+                break;
+            }
+            else
+                k -= chalk[i];
+        }
+        return index;
+    }
+};
