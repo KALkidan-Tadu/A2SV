b82d77602010dd988f85f7eb766e270a5859eff8
KALkidan-Tadu <93763525+KALkidan-Tadu@users.noreply.github.com>
2022-12-23 06:57:48 -0800

    Leetcode Merge sorted array

---
+++ Merge Sorted Array.py
@@ -0,0 +1,22 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        end = n+m-1
+        cur1 = m-1
+        cur2 = n-1
+        while end >= 0:
+            if (cur1>=0 and cur2>=0 and nums1[cur1] >= nums2[cur2]):
+                nums1[end] = nums1[cur1] 
+                cur1 -= 1
+            elif cur1>=0 and cur2>=0:
+                nums1[end] = nums2[cur2]
+                cur2 -= 1
+            elif cur2>=0:
+                nums1[end] = nums2[cur2]
+                cur2-=1
+            else:
+                nums1[end] = nums1[cur1]
+                cur1-=1
+            end -= 1
