89af7f99a5f3363b87a84d79a5b8ce5b5a168ee5
KALkidan-Tadu <93763525+KALkidan-Tadu@users.noreply.github.com>
2022-12-29 13:29:25 -0800

    Smallest Even Multiple from LeetCode

---
+++ Smallest Even Multiple.py
@@ -0,0 +1,8 @@
+class Solution:
+    def smallestEvenMultiple(self, n: int) -> int:
+        i = 1
+        while True:
+            if (n*i) % 2 == 0:
+                return n*i
+            i += 1
+        return 0
