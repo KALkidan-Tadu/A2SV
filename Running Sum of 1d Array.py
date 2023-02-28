63a455420ba5a876bbc3fb3b78ebab6b115ae402
KALkidan-Tadu <93763525+KALkidan-Tadu@users.noreply.github.com>
2023-02-27 22:18:08 -0800

    LeetCode Running Sum of 1d Array

---
+++ Running Sum of 1d Array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        prefixSum = []
+        
+        prefixSum.append(nums[0])
+        for i in range(1, len(nums)):
+            prefixSum.append(prefixSum[i-1] + nums[i])
+        
+        return prefixSum
