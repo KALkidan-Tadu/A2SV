ae971b92d6f82a34e1e009cf2d773671c9924b24
KALkidan-Tadu <93763525+KALkidan-Tadu@users.noreply.github.com>
2023-03-10 09:11:17 -0800

    LeetCode 148

---
+++ Sort List.py
@@ -0,0 +1,53 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    merged = ListNode()
+    temp1 = merged
+
+    def merge(self,list1,list2):
+        if list1 == None or list2 == None:
+            while list1:
+                self.temp1.next = ListNode(list1.val)
+                self.temp1 = self.temp1.next
+                list1 = list1.next
+            while list2:
+                self.temp1.next = ListNode(list2.val)
+                self.temp1 = self.temp1.next
+                list2 = list2.next 
+            return self.merged.next
+        if list1.val <= list2.val:
+            self.temp1.next = ListNode(list1.val)
+            self.temp1 = self.temp1.next
+            list1 = list1.next
+        else:
+            self.temp1.next = ListNode(list2.val)
+            self.temp1 = self.temp1.next
+            list2 = list2.next
+            
+        return self.merge(list1, list2)
+    
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        self.merged = ListNode()
+        self.temp1 = self.merged
+        return self.merge(list1,list2)
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        dummy = ListNode()
+        dummy.next = head
+        slow = dummy
+        fast = dummy
+
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        mid = slow.next
+        slow.next = None
+
+        left = self.sortList(head)
+        right = self.sortList(mid)
+
+        return self.mergeTwoLists(left, right)
