a594b4e7df6f4dd40d5a048400761c7e1036fa1b
KALkidan-Tadu <93763525+KALkidan-Tadu@users.noreply.github.com>
2023-04-09 09:00:32 -0700

    LeetCode 690

--- Employee Importance.py
+++ Employee Importance.py
@@ -1,1 +1,24 @@
+"""
+# Definition for Employee.
+class Employee:
+    def __init__(self, id: int, importance: int, subordinates: List[int]):
+        self.id = id
+        self.importance = importance
+        self.subordinates = subordinates
+"""
 
+class Solution:
+    def getImportance(self, employees: List['Employee'], id: int) -> int:
+        imp = 0
+
+        def dfs(i):
+            nonlocal imp
+            
+            for emp in employees:
+                if emp.id == i:
+                    imp += emp.importance
+                    for sub in emp.subordinates:
+                        dfs(sub)
+        dfs(id)
+        
+        return imp
